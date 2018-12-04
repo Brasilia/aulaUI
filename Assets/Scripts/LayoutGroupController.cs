493c5f9d1cb6756b87fa03bb6cbb406c9d785d79
brasilia <ksuspec@gmail.com>
2018-12-03 22:25:04 -0200

    Added simple page selector effects

---
+++ Assets/Scripts/LayoutGroupController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HorizontalOrVerticalLayoutGroup))]
+public class LayoutGroupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Invoke("DisableLayoutGroup", 0.1f);
+    }
+
+    private void DisableLayoutGroup()
+    {
+        GetComponent<HorizontalOrVerticalLayoutGroup>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/PageSetter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PageSetter : MonoBehaviour, IPointerClickHandler
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnPointerDown(BaseEventData eventData)
+    {
+        
+    }
+
+    public void O(BaseEventData eventData)
+    {
+        
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        transform.parent.GetComponent<HorizontalOrVerticalLayoutGroup>().enabled = false;
+        transform.SetAsLastSibling();
+        foreach(Transform t in transform.parent.GetComponentInChildren<Transform>())
+        {
+            t.localScale = Vector3.one;
+        }
+        transform.localScale = 1.2f * Vector3.one;
+        Debug.Log("Clicked");
+    }
+}
